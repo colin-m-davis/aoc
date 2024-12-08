dcec7e32dd52891e70d8663771e8407c96c6e5ea
colindavis <colinmichaelsdavis@gmail.com>
2024-12-08 11:37:33 -0800

    2025 - 7 part I

---
+++ 2024/7/7_i.py
@@ -0,0 +1,42 @@
+import os
+import itertools
+import functools
+import operator
+
+
+def parse(path):
+    with open(path, 'r') as f:
+        def parse_one(line):
+            first_half, second_half = line.strip().split(': ')
+            test_value = int(first_half)
+            remaining_values = [int(word)
+                                for word in second_half.strip().split(' ')]
+            return (test_value, remaining_values)
+        return [parse_one(line) for line in f.readlines()]
+
+
+def solve(data):
+    return sum(test_value for test_value, remaining_values in data if is_good(test_value, remaining_values))
+
+
+operators = [operator.add, operator.mul]
+
+
+def is_good(test_value, remaining_values):
+    num_operators = len(remaining_values) - 1
+    operator_sequences = itertools.product(operators, repeat=num_operators)
+    return any(apply(remaining_values, sequence) == test_value for sequence in operator_sequences)
+
+
+def apply(values, operators):
+    assert (len(values) == len(operators) + 1)
+
+    def accumulate(accum, item):
+        x, op = item
+        return op(accum, x)
+    return functools.reduce(accumulate, zip(values[1:], operators), values[0])
+
+
+input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'input')
+solution = solve(parse(input_path))
+print(solution)
